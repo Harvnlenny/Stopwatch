b1c259c42fbacd571a661553581ccb51abdcca8f
Aaron Harvey <harvnlenny@gmail.com>
2016-02-13 10:06:30 -0500

    App works great on the emulator (wrapped the timer in a handler) but the timer will reset on an actual device when it is rotated.

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -2,13 +2,61 @@ package com.hfad.stopwatch;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Handler;
+
 
 public class StopwatchActivity extends ActionBarActivity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
-    
+
+    //Start the stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    //Reset the stopwatch when the Reset button is clicked.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    //Sets the number of seconds on the timer.
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = seconds / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
+
+
+
+
+    }
 }
